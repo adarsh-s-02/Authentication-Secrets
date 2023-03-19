31b8a61ecab9f273a0a417205fb31ce088232fba
adarsh s <adarshsetti@gmail.com>
2023-03-19 13:37:36 +0530

    Level 1 : Username and Password Only

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express")
+const ejs = require("ejs")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+
+const app = express()
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser: true});
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+})
+
+
+const User = new mongoose.model("User",userSchema)
+
+app.get("/",function(req,res){
+  res.render("home");
+});
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+app.post("/register",function(req,res){
+  const newUser = new User({
+    email : req.body.username,
+    password : req.body.password
+  })
+  newUser.save()
+  .then(function(){
+    res.render("secrets");
+  })
+  .catch(function(err){
+    console.log(err);
+  });
+});
+
+app.post("/login",function(req,res){
+  const email = req.body.username
+  const password = req.body.password
+  User.findOne({email : email})
+  .then(function(userFound){
+    if(userFound.password === password){
+      res.render("secrets");
+    }else{
+      res.send("Password you have entered is incorrect");
+    }
+  })
+  .catch(function(err){
+    res.send("Please check the email you have entered");
+  });
+});
+
+app.listen(3000,function(){
+  console.log("server started on port 3000");
+})
